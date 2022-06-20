386acf26e71e2c829e2d99bf8008d0e212eae71e
stevenkeezer <stevengkeezer@gmail.com>
2022-06-20 00:59:09 -0700

    fix: button

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
   site_name: 'Starter',
-  title: 'Nextjs Starter',
+  title: 'Home Page | South Carolina Revenue & Fiscal Affairs Office',
   description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
   locale: 'en',
 };
