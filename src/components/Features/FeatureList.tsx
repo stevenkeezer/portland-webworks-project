810c38d7711550012f40abf02ca2495e1faa73b6
stevenkeezer <stevengkeezer@gmail.com>
2022-06-20 23:09:54 -0700

    fix: button

--- src/components/Features/FeatureList.tsx
+++ src/components/Features/FeatureList.tsx
@@ -2,7 +2,7 @@ import { ChevronRightIcon } from '@heroicons/react/solid';
 
 export default function FeatureList({ features }: { features: any[] }) {
   return (
-    <div className="xs:p-6 grid grid-cols-1 gap-2 p-4 p-4 py-6 sm:grid-cols-2">
+    <div className="grid grid-cols-1 gap-2 p-4 py-6 xs:p-6 sm:grid-cols-2">
       {features.map((feature, index) => (
         <div
           tabIndex={0}
@@ -19,7 +19,7 @@ export default function FeatureList({ features }: { features: any[] }) {
               className="uppercase no-underline focus:outline-none"
             >
               <span className="absolute inset-0" aria-hidden="true" />
-              <p className="xs:text-base text-sm font-medium text-blue-800">
+              <p className="text-sm font-medium text-blue-800 xs:text-sm">
                 {feature.name}
               </p>
             </a>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
             />
           </div>
           <div className="relative -mt-4 py-8 px-4 xs:py-16 sm:px-10 sm:py-[6.1rem] lg:px-8">
-            <h1 className="text-center font-roboto text-[1.5rem] font-medium leading-7 tracking-normal text-white xs:leading-7 sm:text-[1.85rem]  sm:leading-10 lg:text-[1.9rem]">
+            <h1 className="text-center font-roboto text-[1.4rem] font-normal leading-7 tracking-normal text-white xs:leading-7 sm:text-[1.85rem]  sm:leading-9 md:leading-10 lg:text-[1.9rem]">
               <span className="mx-auto block max-w-[45rem] sm:px-16 md:px-0">
                 Providing independent research, analysis, and resources to
                 facilitate informed policy decisions
@@ -24,7 +24,7 @@ export default function Hero() {
               <span>and administration of services</span>
             </h1>
 
-            <div className="mx-auto pt-7 xs:px-2 sm:flex sm:justify-center sm:px-10 md:max-w-none md:px-24">
+            <div className="mx-auto pt-7 xs:px-2 sm:flex sm:justify-center sm:px-10 md:max-w-4xl md:px-32">
               <SearchBar />
             </div>
           </div>
