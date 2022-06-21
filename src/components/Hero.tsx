3bd3bff255b37ca2a7b2e54788b27a52c4564123
stevenkeezer <stevengkeezer@gmail.com>
2022-06-20 23:03:10 -0700

    fix: button

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,15 +16,15 @@ export default function Hero() {
             />
           </div>
           <div className="relative -mt-4 py-8 px-4 xs:py-16 sm:px-10 sm:py-[6.1rem] lg:px-8">
-            <h1 className="text-center font-roboto text-[1.4rem] leading-7 tracking-normal text-white xs:text-[1.55rem] xs:leading-7  sm:leading-9 lg:text-[1.65rem]">
+            <h1 className="text-center font-roboto text-[1.5rem] font-medium leading-7 tracking-normal text-white xs:leading-7 sm:text-[1.85rem]  sm:leading-10 lg:text-[1.9rem]">
               <span className="mx-auto block max-w-[45rem] sm:px-16 md:px-0">
                 Providing independent research, analysis, and resources to
-                facilitate informed policy decisions and
+                facilitate informed policy decisions
               </span>
-              <span>administration of services</span>
+              <span>and administration of services</span>
             </h1>
 
-            <div className="mx-auto pt-7 xs:px-6 sm:flex sm:justify-center sm:px-20 md:max-w-none md:px-24">
+            <div className="mx-auto pt-7 xs:px-2 sm:flex sm:justify-center sm:px-10 md:max-w-none md:px-24">
               <SearchBar />
             </div>
           </div>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -16,9 +16,9 @@ class MyDocument extends Document {
             crossOrigin="true"
           />
           <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto+Slab:wght@400;700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Roboto+Condensed&family=Roboto+Slab:wght@400;500;700&display=swap"
             rel="stylesheet"
-          />
+          ></link>
         </Head>
         <body>
           <Main />
